9c4bcfb8560e24b447d9af520bb69c5584beddc4
SKeeneCode <samkeene@protonmail.com>
2018-12-22 14:34:01 +0000

    edit for online https use

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const errorhandler = require('errorhandler');
+const https = require('https');
+const fs = require('fs');
+path = require('path');
 
 let store = {
     posts: []
 };
 
+let key = fs.readFileSync(path.join(__dirname,'../../ssl/keys/c07f7_003d1_ecb06ccb2afcd72cfa43b6011c82464e.key'));
+let cert = fs.readFileSync(path.join(__dirname,'../../ssl/certs/samkeene_co_uk_c07f7_003d1_1574035199_5b90fc5e96ac0c534d2ee116af6fd342.crt'));
+let options = {
+    key: key,
+    cert: cert
+};
 
 let app = express();
 app.use(bodyParser.json());
@@ -17,6 +26,10 @@ app.use(errorhandler());
 app.use((req, res, next) => {
     req.store = store;
     next();
+});
+
+app.get('/', (req, res) => {
+    res.send('hello world')
 });
 
 app.get('/posts', routes.Posts.getPosts);
@@ -29,6 +42,10 @@ app.post('/posts/:postID/comments/', routes.Comments.addComment);
 app.put('/posts/:postID/comments/:commentID', routes.Comments.updateComment);
 app.delete('/posts/:postID/comments/:commentID', routes.Comments.removeComment);
 
-app.listen(3000);
+let server = https.createServer(options, app);
+
+server.listen(3000, () => {
+    console.log("server starting on port : " + 3000)
+});
 
 
